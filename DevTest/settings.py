bf78231ee1c77212d4e16216a7788bd1e1c97945
Karamveer Singh <karamveersingh2003111@Gmail.com>
2024-10-19 22:40:13 +0530

    updated the settings.py

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -127,6 +127,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'karamveersingh2003111@gmail.com'
-EMAIL_HOST_PASSWORD = 'gttp gmel yfmp gxoe'  # Ensure you've set this up properly
+EMAIL_HOST_USER = '<your_email_here>'
+EMAIL_HOST_PASSWORD = '<put_your_password_here>'  # Ensure you've set this up properly
 
